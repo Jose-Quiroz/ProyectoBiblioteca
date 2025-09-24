411b0081d55fc4fa10f6fe161de1867dee2f3392
Ludw-FT <julioludw.2016@gmail.com>
2025-09-24 13:28:36 -0500

    feat: agrega mensaje de prueba para PR

--- Prueba.java
+++ Prueba.java
@@ -1,6 +1,6 @@
 public class Prueba {
     public static void main(String[] args) {
-        System.out.println("prueba pull request");
+        System.out.println("prueba  pull request");
 
     }
 
